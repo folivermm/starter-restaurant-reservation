712223ef48661af0a4264496aba040f8674242c5
folivermm <foliverm@gmail.com>
2023-10-13 13:04:10 -0700

    reservation and list Reservation

---
+++ front-end/src/reservations/Reservation.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { cancelReservation } from "../utils/api";
+
+function Reservation({
+    reservation_id,
+    first_name,
+    last_name,
+    mobile_number,
+    reservation_date,
+    reservation_time,
+    people,
+    loadDashboard,
+}) {
+    function cancelHandler(event, reservation_id) {
+        event.preventDefault();
+        const notification = "Do you want to cancel this reservation?";
+        if (window.confirm(notification)) {
+            cancelReservation(reservation_id, "cancelled").then(() =>
+                loadDashboard()
+            );
+        }
+    }
+
+    return (
+        <div className="card mb-3" key={reservation_id}>
+            <div className="card-body">
+                <p className="card-text">
+                    Name: {first_name} {last_name}
+                </p>
+                <p className="card-text">Mobile number: {mobile_number}</p>
+                <p className="card-text">Date: {reservation_date}</p>
+                <p className="card-text">Time: {reservation_time}</p>
+                <p className="card-text">Party Size: {people} </p>
+                <p className="card-text">Reservation ID: {reservation_id}</p>
+                <button
+                    className="btn btn-danger m-1"
+                    data-reservation-id-cancel={reservation_id}
+                    onClick={(event) => cancelHandler(event, reservation_id)}
+                >
+                    Cancel
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default Reservation;
